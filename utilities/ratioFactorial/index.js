96e0c91b0965591bc5933fa1d7de1b225600b3bb
saiteja <loveui19@gmail.com>
2024-01-24 08:37:30 +0000

    Calculator Rati and Factorail

--- utilities/ratioFactorial/index.js
+++ utilities/ratioFactorial/index.js
@@ -4,7 +4,6 @@ const factorialOfNumber = require('../factorial/index')
 const ratioAndFactorial = (num1, num2, num3) => {
   const ratio = ratioOfTwoNumbers(num1, num2)
   const factorial = factorialOfNumber(num3)
-
   return {ratio, factorial}
 }
 
